2a5a504f75365dfd42159f22cd7dc5174eb398d6
Christian Dallago <code@dallago.us>
2016-03-09 23:57:53 +0100

    changed token declaration

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ var context;
 
 module.exports = {
     start: function(done) {
-        console.log('---------- START BOOTSTRAPPING ----------');
 
         // Import modules
-        console.log('Importing dependencies...');
         const express = require('express');
         const bodyParser = require('body-parser');
         const q = require('q');
@@ -30,7 +28,6 @@ module.exports = {
         const fs = require('fs');
 
         const config = require('./configuration');
-        console.log('Dependencies has been imported successfully');
 
         // Initialize the context
         context = {};
@@ -73,95 +70,95 @@ module.exports = {
                 return;
             }
             console.log('Connection test to MySQL worked.');
-            console.log('Connected as id ' + connection.threadId);
-
-
-            // Initialize express
-            context.app = express();
-            context.app.use(bodyParser.json());
-            context.app.use(bodyParser.urlencoded({extended: true}));
-
-            // Initialize nodemailer
-            context.smtpTransporter = nodemailer.createTransport(config.email.defaultFrom);
-
-            // SetUp default email sender and subject from configuration
-            context.mailOptions = function(){
-                return {
-                    from: config.email.defaultFrom,
-                    subject: config.email.defaultSubject
-                };
-            };
-
-            // verify connection configuration 
-            context.smtpTransporter.verify(function(error, success) {
-                if (error) {
-                    console.log("This node instance won't be able to send emails! Error: " + error);
-                } else {
-                    console.log("This node instance is able to send emails.");
-                }
-            });
-
-            // Initialize router
-            context.router = new express.Router();
-
-            // Response for the 'home request'
-            context.app.get('/', function(request, response) {
-                response.send('<html><body>It works! The API is available under <strong>/api/*</strong></body></html>');
-            });
-
-            // Set router for the whole api
-            context.app.use('/api', context.router);
-            context.router.use(function(request, response, next) {
-
-                // Log each request to the console
-                console.log(request.method, request.url);
-
-                // Check that the head request contains the token in config or generate a new token.
-                var token = config.accessToken;
-                if(!token){
-                    token = context.uuid.v4();
-                }
-
-                console.log('Your requests must contain the token'+ token);
-
-                context.token = token;
-                // Continue doing what we were doing and go to the route
-                return next();
-            });
-
-            // Function to load all components from the respective folders (models, controllers, services, daos, utils)
-            context.component = function(componentName) {
-                if (!context[componentName]) {
-                    context[componentName] = {};
-                }
-
-                return {
-                    module: function(moduleName) {
-                        if (!context[componentName][moduleName]) {
-                            console.log('Loading component ' + componentName);
-                            context[componentName][moduleName] = require(path.join(__dirname, componentName, moduleName))(context,
-                                                                                                                          componentName, moduleName);
-                            console.log('LOADED ' + componentName + '.' + moduleName);
-                        }
-
-                        return context[componentName][moduleName];
-                    }
-                };
-            };
-
-            // Initialize routes module in order to cover all services
-            // ALso kicks off loading of all modules
-            context.component('controllers').module('routes');
-
-            // Done, start server
-            console.log('Starting server...');
-            context.server = context.app.listen(context.config.app.port, function() {
-                console.log('SERVER LISTENING ON PORT %s', context.config.app.port);
-                console.log('---------- DONE BOOTSTRAPPING ----------');
-                done(context);
-            });
-            connection.release();
         });
+
+        // Initialize express
+        context.app = express();
+        context.app.use(bodyParser.json());
+        context.app.use(bodyParser.urlencoded({extended: true}));
+
+        // Initialize nodemailer
+        context.smtpTransporter = nodemailer.createTransport(config.email.defaultFrom);
+
+        // SetUp default email sender and subject from configuration
+        context.mailOptions = function(){
+            return {
+                from: config.email.defaultFrom,
+                subject: config.email.defaultSubject
+            };
+        };
+
+        // verify connection configuration 
+        context.smtpTransporter.verify(function(error, success) {
+            if (error) {
+                console.log("This node instance won't be able to send emails! Error: " + error);
+            } else {
+                console.log("This node instance is able to send emails.");
+            }
+        });
+
+        // Initialize router
+        context.router = new express.Router();
+
+        // Response for the 'home request'
+        context.app.get('/', function(request, response) {
+            response.send('<html><body>It works! The API is available under <strong>/api/*</strong></body></html>');
+        });
+
+        // Set up the token that needs to be passed in the head with every request
+        var token = config.app.accessToken;
+        if(!token){
+            token = context.uuid.v4();
+        }
+
+        console.log('Your requests must contain the following token:');
+        console.log(token);
+
+        context.token = token;
+
+        // Set router for the whole api
+        context.app.use('/api', context.router);
+        context.router.use(function(request, response, next) {
+
+            // Log each request to the console
+            console.log(request.method, request.url);
+
+            // Continue doing what we were doing and go to the route
+            return next();
+        });
+
+        // Function to load all components from the respective folders (models, controllers, services, daos, utils)
+        context.component = function(componentName) {
+            if (!context[componentName]) {
+                context[componentName] = {};
+            }
+
+            return {
+                module: function(moduleName) {
+                    if (!context[componentName][moduleName]) {
+                        console.log('Loading component ' + componentName);
+                        context[componentName][moduleName] = require(path.join(__dirname, componentName, moduleName))(context,
+                                                                                                                      componentName, moduleName);
+                        console.log('LOADED ' + componentName + '.' + moduleName);
+                    }
+
+                    return context[componentName][moduleName];
+                }
+            };
+        };
+
+        // Initialize routes module in order to cover all services
+        // ALso kicks off loading of all modules
+        context.component('controllers').module('routes');
+
+        // Done, start server
+        console.log('Starting server...');
+        context.server = context.app.listen(context.config.app.port, function() {
+            console.log('SERVER LISTENING ON PORT %s', context.config.app.port);
+            console.log('---------- DONE BOOTSTRAPPING ----------');
+            done(context);
+        });
+
     },
 
     stop: function(done) {
