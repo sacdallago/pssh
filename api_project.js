0af41f2ccff58b0a57186e75fe8085e761cdf3d4
Christian Dallago <code@dallago.us>
2016-08-17 18:18:07 +0800

    docs v0.0.1

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "pssh API",
+  "version": "0.1.0",
+  "description": "This API enables to spawn pssh calculations",
+  "title": "pssh API",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-17T10:14:51.326Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
