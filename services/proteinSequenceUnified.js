0437aebee738c86ee08cbe3ebbd8a7e04dbce1a1
Christian Dallago <code@dallago.us>
2016-08-17 18:16:36 +0800

    Started apidoc documentation

--- services/proteinSequenceUnified.js
+++ services/proteinSequenceUnified.js
@@ -13,6 +13,100 @@ module.exports = function(context) {
 
     // Routes
     context.router
+        /**
+         * @api {get} /md5/:md5 Request protein sequence information
+         * @apiVersion 0.1.0
+         * @apiName getMd5
+         * @apiGroup Get
+         * @apiDescription Returns the entry from `protein_sequence_unified` if `:md5` matches an entry, or error instead.
+         * @apiParam {String} md5 Protein sequence MD5 hash.
+         *
+         * @apiSuccessExample {json} Success Response No Pssh
+         *HTTP/1.1 200
+         *{
+         *  "data": [
+         *    {
+         *       "Primary_Accession": null,
+         *       "Sequence": "acb",
+         *       "MD5_Hash": "hash",
+         *       "Description": null,
+         *       "Length": 3,
+         *      "pssh_finished": 0
+         *    }
+         *  ],
+         *  "count": 1
+         *}
+         * @apiSuccessExample {json} Success Response has Pssh
+         *HTTP/1.1 200
+         *{
+         *  "data": [
+         *    {
+         *       "Primary_Accession": "123",
+         *       "Sequence": "acb",
+         *       "MD5_Hash": "hash",
+         *       "Description": "abc",
+         *       "Length": 3,
+         *      "pssh_finished": 1
+         *    }
+         *  ],
+         *  "count": 1
+         *}
+         * @apiError (404) NotFound No data for the given protein found.
+         * @apiErrorExample {json} NotFound
+         *HTTP/1.1 404
+         *{
+         *  "data": [],
+         *  "count": 0
+         *}
+         * @apiError (500) InternalServerError Internal node error.
+         */
         .get('/md5/:md5', proteinSequenceUnifiedController.getMD5)
+                /**
+         * @api {get} /sequence/:sequence Request protein sequence information
+         * @apiVersion 0.1.0
+         * @apiName getSequence
+         * @apiGroup Get
+         * @apiDescription Returns the entry from `protein_sequence_unified` if `:sequence` matches an entry, or error instead.
+         * @apiParam {String} sequence Protein sequence.
+         *
+         * @apiSuccessExample {json} Success Response No Pssh
+         *HTTP/1.1 200
+         *{
+         *  "data": [
+         *    {
+         *       "Primary_Accession": null,
+         *       "Sequence": "acb",
+         *       "MD5_Hash": "hash",
+         *       "Description": null,
+         *       "Length": 3,
+         *      "pssh_finished": 0
+         *    }
+         *  ],
+         *  "count": 1
+         *}
+         * @apiSuccessExample {json} Success Response has Pssh
+         *HTTP/1.1 200
+         *{
+         *  "data": [
+         *    {
+         *       "Primary_Accession": "123",
+         *       "Sequence": "acb",
+         *       "MD5_Hash": "hash",
+         *       "Description": "abc",
+         *       "Length": 3,
+         *      "pssh_finished": 1
+         *    }
+         *  ],
+         *  "count": 1
+         *}
+         * @apiError (404) NotFound No data for the given protein found.
+         * @apiErrorExample {json} NotFound
+         *HTTP/1.1 404
+         *{
+         *  "data": [],
+         *  "count": 0
+         *}
+         * @apiError (500) InternalServerError Internal node error.
+         */
         .get('/sequence/:sequence', proteinSequenceUnifiedController.getSequence);
 };
--- services/proteinSequenceUser.js
+++ services/proteinSequenceUser.js
@@ -13,6 +13,82 @@ module.exports = function(context) {
 
     // Routes
     context.router
+        /**
+         * @api {post} /sequence Submit a sequence to the database in the `user_sequences` table
+         * @apiVersion 0.1.0
+         * @apiName postSequence
+         * @apiGroup Post
+         * @apiDescription Inserts a sequence to be processes via PSSH. The calls can be formatted as `multipart/form-data`, `application/x-www-form-urlencoded` or `application/json`.
+         * @apiSuccess {Object} nested An Object.
+         * @apiParam {String} nested.email Email of the submitter.
+         * @apiParam {String} nested.sequence Sequence to be uploaded
+         * @apiParam {Binary} nested.pssh [OPTIONAL] pssh calculation of `sequence` with corresponding md5 hash.
+         *
+         * @apiHeader {String} token The `token` neccessary to perform POST requests, defined in the configuration.js
+         *
+         * @apiSuccessExample {json} Created
+         *HTTP/1.1 201
+         *{
+         *  "fieldCount": 0,
+         *  "affectedRows": 1,
+         *  "insertId": 0,
+         *  "serverStatus": 2,
+         *  "warningCount": 0,
+         *  "message": "",
+         *  "protocol41": true,
+         *  "changedRows": 0
+         *}
+         * @apiError (422) UnprocessableEntity Most likely duplicate entry, see example below
+         * @apiErrorExample {json} UnprocessableEntity 
+         *HTTP/1.1 422
+         *{
+         *  "code": "ER_DUP_ENTRY",
+         *  "errno": 1062,
+         *  "sqlState": "23000",
+         *  "index": 0
+         *}
+         * @apiError (415) UnsuportedMediaType Wrong method. Allowed methods: `multipart/form-data`, `application/x-www-form-urlencoded` or `application/json`.
+         * @apiError (401) Unauthorized Wrong or not token.
+         * @apiError (500) InternalServerError Internal node error.
+         */
         .post('/sequence', proteinSequenceUserController.psshFromSequence)
+        /**
+         * @api {post} /sequence Submit a sequence to the database in the `user_sequences` table
+         * @apiVersion 0.1.0
+         * @apiName postSequence
+         * @apiGroup Post
+         * @apiDescription Inserts a sequence to be processes via PSSH. The calls can be formatted as `multipart/form-data`, `application/x-www-form-urlencoded` or `application/json`.
+         * @apiSuccess {Object} nested An Object.
+         * @apiParam {String} nested.email Email of the submitter.
+         * @apiParam {String} nested.sequence Sequence to be uploaded
+         * @apiParam {Binary} nested.pssh [OPTIONAL] pssh calculation of `sequence` with corresponding md5 hash.
+         *
+         * @apiHeader {String} token The `token` neccessary to perform POST requests, defined in the configuration.js
+         *
+         * @apiSuccessExample {json} Created
+         *HTTP/1.1 201
+         *{
+         *  "fieldCount": 0,
+         *  "affectedRows": 1,
+         *  "insertId": 0,
+         *  "serverStatus": 2,
+         *  "warningCount": 0,
+         *  "message": "",
+         *  "protocol41": true,
+         *  "changedRows": 0
+         *}
+         * @apiError (422) UnprocessableEntity
+         * @apiErrorExample {json} UnprocessableEntity
+         *HTTP/1.1 422
+         *{
+         *  "code": "ER_DUP_ENTRY",
+         *  "errno": 1062,
+         *  "sqlState": "23000",
+         *  "index": 0
+         *}
+         * @apiError (415) UnsuportedMediaType
+         * @apiError (401) Unauthorized
+         * @apiError (500) InternalServerError
+         */
         .post('/fasta', proteinSequenceUserController.psshFromFasta);
 };
